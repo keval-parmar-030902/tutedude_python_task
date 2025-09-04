a53ca6a9e70a5970dbd16d5b957f3efa3db1f7e2
keval-parmar-030902 <kevalparmar030902@gmail.com>
2025-09-04 15:07:01 +0530

    Assignment 1 Complete

---
+++ Assignment_1/Task1.py
@@ -0,0 +1,11 @@
+'''
+Simple Calculator to perform basic calculator
+'''
+
+num1 = int(input("Enter the First Number: "))
+num2 = int(input("Enter the Second Number: "))
+
+print("Addition :", num1 + num2)
+print("Substraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division :", num1 / num2)
---
+++ Assignment_1/Task2.py
@@ -0,0 +1,9 @@
+'''
+Create Personalize Gretting
+'''
+
+fname = input("Enter Your First name: ")
+lname = input("Enter Your Last name: ")
+
+
+print(f'Hello, {fname} {lname}! Welcome to the Python program.')
